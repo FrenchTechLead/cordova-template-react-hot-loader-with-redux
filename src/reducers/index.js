51cce0e1091be0c736308ec1fa82e87afaa34971
Meshredded <mecheri.akram@gmail.com>
2017-06-25 00:38:52 +0200

    README

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,7 @@
 import {combineReducers} from 'redux';
 import helloReducer from "./HelloReducer";
 
+// you can create other reducers and put them in the combine Reducers middleWare.
 export default combineReducers({
   helloReducer
 })
